a33dcf141811cbf4dee0268c13d56f6bad56ae01
owenschu7 <owenschu7@gmail.com>
2026-09-17 14:03:03 -0700

    changed debug to true

--- owenschu/owenschu/settings.py
+++ owenschu/owenschu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR.parent / '.env')
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-only')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 CSRF_TRUSTED_ORIGINS = ['https://*.run.app']
 
